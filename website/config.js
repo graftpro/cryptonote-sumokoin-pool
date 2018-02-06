1bb7059aad5eee0457004b044621117c3bcc4b2c
Jonathan <jspeigner@users.noreply.github.com>
2018-02-06 12:25:03 -0500

    Update config.js
    Changed to GraftPro.com

--- website/config.js
+++ website/config.js
@@ -1,17 +1,17 @@
-var api = "https://pool.sumokoin.ch:8888";
+var api = "https://us.pool.graftpro.com:8888";
 
-var poolHost = "pool.sumokoin.ch";
+var poolHost = "pool.graftpro.com";
 
 var irc = "irc.freenode.net/#sumokoin";
 
-var email = "support@sumokoin.ch";
+var email = "support@graftpro.com";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://explorer.sumokoin.com/block/{id}";
+var blockchainExplorer = "http://explorer.graftpro.com/block/{id}";
 
-var transactionExplorer = "http://explorer.sumokoin.com/tx/{id}";
+var transactionExplorer = "http://explorer.graftpro.com/tx/{id}";
 
 var themeCss = "themes/default-theme.css";
